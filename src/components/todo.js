2e0be029933a71752b919d7fe4549037381d9c57
CletwinE <ecletwin@gmail.com>
2022-09-18 00:32:44 +0530

    Intial Commit

---
+++ src/components/todo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const todo = () => {
+    return (
+        <div className='header text-center'>
+            <h3>ToDo List</h3>
+            <button className='btn btn-primary mt-2'>Add List</button>
+        </div>
+    );
+};
+
+export default todo;
