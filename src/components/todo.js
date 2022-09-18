9f430110e86f188fc5a4eaa6229822308236e74b
CletwinE <ecletwin@gmail.com>
2022-09-18 22:30:39 +0530

    Final Commit

--- src/components/todo.js
+++ src/components/todo.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import CreateTask from '../models/CreateTaskModel';
 import Card from './card';
 import Clock from 'react-live-clock';
-import pic from '../images/nodata.png'
+import pic from '../images/nodata.png';
+import bg from '../images/todo.jpg'
 
 const Todo = () => {
 
@@ -50,11 +51,12 @@ const Todo = () => {
                     <div className='clock text-center'>
                         <Clock format={'MMMM Do YYYY, h:mm:ss a'} ticking={true} timezone={'Asia/Colombo'} />
                     </div>
-                    <h2 className='pt-2'>ToDo List</h2>
+                    <h2 className='pt-4'>TODOs </h2>
                     <button className='btn btn-primary mt-2' onClick={() => setModal(true)}>Add List</button>
                 </div>
 
-                <div className='task-container'>
+                <div className='task-container todobody'>
+                    <img className='bg' src={bg}/>
                     {taskList && taskList.map((obj, index) => <Card taskObj={obj} index={index} deleteTask={deleteTask} updateListArray={updateListArray} />)}
                 </div>
 
@@ -70,8 +72,8 @@ const Todo = () => {
                     <div className='clock text-center'>
                         <Clock format={'MMMM Do YYYY, h:mm:ss a'} ticking={true} timezone={'Asia/Colombo'} />
                     </div>
-                    <h2 className='pt-2'>ToDo List</h2>
-                    <button className='btn btn-primary mt-2' onClick={() => setModal(true)}>Add List</button>
+                    <h2 className='todo pt-4'>ToDo List</h2>
+                    <button className='btn btn-primary mt-3' onClick={() => setModal(true)}>Add New ToDo</button>
                 </div>
 
                 <div className='task-container imagecont'>
