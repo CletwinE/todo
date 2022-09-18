c0ad292ce299ed46e3c78a411e7bb0d42661458c
CletwinE <ecletwin@gmail.com>
2022-09-18 21:51:46 +0530

    Updated

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import CreateTask from '../models/CreateTaskModel';
 import Card from './card';
-import Clock from 'react-live-clock'
+import Clock from 'react-live-clock';
+import pic from '../images/nodata.png'
 
 const Todo = () => {
 
@@ -41,25 +42,52 @@ const Todo = () => {
         }
     }, []);
 
-    return (
 
-        <>
-            <div className='header text-center'>
-                <div className='clock text-center'>
-                    <Clock format={'MMMM Do YYYY, h:mm:ss a'} ticking={true} timezone={'Asia/Colombo'} />
+    if (taskList.length != 0) {
+        return (
+            <>
+                <div className='header text-center'>
+                    <div className='clock text-center'>
+                        <Clock format={'MMMM Do YYYY, h:mm:ss a'} ticking={true} timezone={'Asia/Colombo'} />
+                    </div>
+                    <h2 className='pt-2'>ToDo List</h2>
+                    <button className='btn btn-primary mt-2' onClick={() => setModal(true)}>Add List</button>
                 </div>
-                <h2 className='pt-2'>ToDo List</h2>
-                <button className='btn btn-primary mt-2' onClick={() => setModal(true)}>Add List</button>
-            </div>
 
-            <div className='task-container'>
-                {taskList && taskList.map((obj, index) => <Card taskObj={obj} index={index} deleteTask={deleteTask} updateListArray={updateListArray} />)}
-            </div>
-            
-            <CreateTask modal={modal} toggle={toggle} save={saveTask} />
-        </>
+                <div className='task-container'>
+                    {taskList && taskList.map((obj, index) => <Card taskObj={obj} index={index} deleteTask={deleteTask} updateListArray={updateListArray} />)}
+                </div>
 
-    );
+                <CreateTask modal={modal} toggle={toggle} save={saveTask} />
+            </>
+
+        );
+    }
+    else {
+        return (
+            <>
+                <div className='header text-center'>
+                    <div className='clock text-center'>
+                        <Clock format={'MMMM Do YYYY, h:mm:ss a'} ticking={true} timezone={'Asia/Colombo'} />
+                    </div>
+                    <h2 className='pt-2'>ToDo List</h2>
+                    <button className='btn btn-primary mt-2' onClick={() => setModal(true)}>Add List</button>
+                </div>
+
+                <div className='task-container imagecont'>
+                    <div>
+                        <img className='image' src={pic}/>
+                    </div>
+                </div>
+
+                <CreateTask modal={modal} toggle={toggle} save={saveTask} />
+            </>
+
+        );
+    }
+
+
+
 };
 
 export default Todo;
